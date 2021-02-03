8de9b09cb93807fc0148f45aa133b5694435b8df
Vera <schoenigervera@gmail.com>
2021-02-03 18:10:16 +0100

    Update CardManager.cs
    - fixed right margin of card layout not being repected
    - changed margin to a fixed value

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -9,7 +9,7 @@ public class CardManager : MonoBehaviour
     private Camera mainCamera;
     private int cameraHeight;
     private int cameraWidth;
-    private readonly float cardMargin = .2f; ///the margin as a fraction of the cardsize
+    private readonly float cardMargin = .2f; 
     public GameObject[] cards;
     private Vector2 cameraSize;
 
@@ -101,15 +101,21 @@ public class CardManager : MonoBehaviour
     {
         Vector2 cardSize = cards[0].GetComponent<SpriteRenderer>().sprite.bounds.size;
         Vector2 screenSizeWorld = mainCamera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
-        Vector2 effectiveSpace = new Vector2(screenSizeWorld.x - 2 * cardSize.x * cardMargin, screenSizeWorld.y - 2 * cardSize.y * cardMargin);
+        print(screenSizeWorld);
+        Vector2 effectiveSpace = new Vector2(screenSizeWorld.x - 2 * cardMargin, screenSizeWorld.y - 2 * cardMargin);
+        print(effectiveSpace);
 
+        float cardSpace = cards.Length * ((cardSize.x + cardMargin)* (cardSize.y + cardMargin));
+        print(cardSpace);
 
-        float cardSpace = (cards.Length * (cardSize.x + cardSize.x * cardMargin))* (cardSize.y + cardSize.y * cardMargin);
-        float cardSizeFactor = effectiveSpace.x * effectiveSpace.y / cardSpace;
-        Vector2 cardSizeScaled = new Vector2(cardSize.x * cardSizeFactor, cardSize.y * cardSizeFactor);
+        float cardSizeFactor = (effectiveSpace.x * effectiveSpace.y / cardSpace);
+        print(cardSizeFactor);
+
+        Vector2 cardSizeScaled = new Vector2(cardSize.x * cardSizeFactor - cardMargin, cardSize.y * cardSizeFactor - cardMargin);
+
         Vector2 cardStartPos = mainCamera.ScreenToWorldPoint(new Vector3(0, cameraHeight));
-        cardStartPos += new Vector2(cardSizeScaled.x * cardMargin * 2 + cardSizeScaled.x / 2, 
-                                    -(cardSizeScaled.y * cardMargin * 2 + cardSizeScaled.y / 2));
+        cardStartPos += new Vector2(cardMargin * 2 + cardSizeScaled.x / 2, 
+                                    -(cardMargin * 2 + cardSizeScaled.y / 2));
         Vector2 nextCardPos = cardStartPos;
 
 
@@ -117,13 +123,17 @@ public class CardManager : MonoBehaviour
         foreach (GameObject card in cards)
         {
             card.transform.localScale = cardSizeScaled;
-            card.transform.position = nextCardPos;
-            nextCardPos += new Vector2(cardSizeScaled.x + cardSizeScaled.x * cardMargin, 0f);
-            if (nextCardPos.x > mainCamera.ScreenToWorldPoint(new Vector3(cameraWidth,cameraHeight,0f)).x)
+            if (nextCardPos.x + cardSizeScaled.x/2 + cardMargin/2 > effectiveSpace.x)
             {
-                nextCardPos = new Vector2(cardStartPos.x, nextCardPos.y - (cardSizeScaled.y + cardSizeScaled.y * cardMargin));
-            }
+                nextCardPos = new Vector2(cardStartPos.x, nextCardPos.y - (cardSizeScaled.y + cardMargin));
+                card.transform.position = nextCardPos;
 
+            }
+            else
+            {
+                card.transform.position = nextCardPos;
+                nextCardPos += new Vector2(cardSizeScaled.x + cardMargin, 0f);
+            }
         }
 
 
