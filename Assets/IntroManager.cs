5adfe655749644514284fe69d3af3cc157cd4d50
Vera <schoenigervera@gmail.com>
2022-01-25 12:53:16 +0100

    Intro manager & sprites
    - wrote intro manager to handle video + titlecard timings
    - fixed titlecard layout size
    - added high res big card sprites and exchanged which sprites are in use
    - fixed blowing leaves being blurred by DOF effect
    - added cursor and icon

---
+++ Assets/IntroManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class IntroManager : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public Canvas canvas;
+    public float showTitle = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(IntroTiming());
+    }
+
+    private IEnumerator IntroTiming()
+    {
+        yield return new WaitForSeconds((float)videoPlayer.clip.length);
+        videoPlayer.gameObject.SetActive(false);
+        canvas.gameObject.SetActive(true);
+        yield return new WaitForSeconds(showTitle);
+        SceneManager.LoadSceneAsync(1);
+    }
+}
