abb8a46e1f7928e165f560d4ff36b49e93742942
Vera <schoenigervera@gmail.com>
2022-01-31 10:55:36 +0100

    Texts & Link
    - added Hyperlink in menu
    - adjusted some texts

---
+++ Assets/Scripts/OpenLink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void OpenHyperlink(string link)
+    {
+        Application.OpenURL(link);
+    }
+}
